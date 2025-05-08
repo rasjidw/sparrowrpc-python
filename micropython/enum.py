47f8a7bb3f2216d329fe53f26f6794d446c20cca
Rasjid Wilcox <rasjidw@openminddev.net>
2025-05-09 06:31:48 +1000

    Get micropython StrEnum to raise a ValueError on invalid enums.

--- micropython/enum.py
+++ micropython/enum.py
@@ -1,5 +1,23 @@
 class StrEnum(str):
+    _valid_values = set()
+
     def __new__(cls, value):
+        if value not in cls.get_valid_values():
+            raise ValueError(f'{value!r} is not a valid {cls.__name__}')
         member = str(value)
         return member
     
+    @classmethod
+    def get_valid_values(cls):
+        if cls._valid_values:
+            return cls._valid_values
+        
+        valid_values = set()
+        for key, key_value in cls.__dict__.items():
+            if key[0] == '_':
+                continue
+            if not isinstance(key_value, str):
+                continue
+            valid_values.add(key_value)
+        cls._valid_values = valid_values
+        return valid_values
