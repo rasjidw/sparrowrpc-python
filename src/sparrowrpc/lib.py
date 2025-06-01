ee65844e5bfeef8f8c8cb8d1fdb9897c26be136f
Rasjid Wilcox <rasjidw@openminddev.net>
2025-06-01 18:07:42 +1000

    Class to install a signal handler.

---
+++ src/sparrowrpc/lib.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import logging
+import signal
+import sys
+from typing import Optional, Iterable
+
+
+log = logging.getLogger(__name__)
+
+
+class SignalHandlerInstaller:
+    def __init__(self, signals: Optional[Iterable[signal.Signals]] = ()):
+        if not signals:
+            if sys.platform == 'win32':
+                signals = [signal.SIGINT, signals.SIGBREAK]
+            else:
+                signals = [signal.SIGINT, signal.SIGTERM]
+        self.signals = signals
+        self.saved_handlers = dict()  # signal -> handler
+
+    def install(self, handler: callable):
+        for sig in self.signals:
+            self.saved_handlers[sig] = signal.getsignal(sig)
+            signal.signal(sig, handler)
+    
+    def remove(self):
+        for sig in self.signals:
+            signal.signal(sig, self.saved_handlers[sig])
